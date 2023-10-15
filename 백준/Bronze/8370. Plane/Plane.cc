8100a37742a2ee58e9fa6b3d03d7458a90b2a458
jeongsoo <102466913+kimjeongsoo20190147@users.noreply.github.com>
2023-10-15 23:41:27 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1,k1,n2,k2;
+    cin >> n1 >> k1 >> n2 >> k2;
+    
+    int sum;
+    sum = (n1*k1) + (n2*k2);
+    
+    cout << sum;
+    return 0;
+}
