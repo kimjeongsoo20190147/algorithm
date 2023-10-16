772ad5b0652b79a2d533b57b5ebd207d070add2e
jeongsoo <102466913+kimjeongsoo20190147@users.noreply.github.com>
2023-10-16 15:21:48 +0900

    [Bronze IV] Title: Hello Judge, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, i;
+    cin >> a;
+    i=1;
+    
+    for (i=1; i<a+1;i++){
+        cout << "Hello World, Judge " << i << "!" << endl;
+    }
+    
+    return 0;
+    
+}
