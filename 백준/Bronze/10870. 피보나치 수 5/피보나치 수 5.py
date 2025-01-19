fbbfd3ca14367bf8476ca38f873255cde2d46031
jeongsoo <102466913+kimjeongsoo20190147@users.noreply.github.com>
2025-01-19 16:19:48 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,11 +1,17 @@
 n = int(input())
 
-def fibo(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    else:
-        return fibo(n-1) + fibo(n-2)
+arr = [-1]*(n+2)
+arr[0]=0
+arr[1]=1
+
+def fibo(x):
+    global arr
+    
+    if arr[x] != -1:
+        return arr[x]
+    
+    arr[x] = fibo(x-1) + fibo(x-2)
+    return arr[x]
+
 
 print(fibo(n))
