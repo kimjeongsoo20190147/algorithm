47b758324426c86003d03f2259ba4efb4153df7a
kimjeongsoo <102466913+kimjeongsoo20190147@users.noreply.github.com>
2025-08-07 20:55:13 +0900

    [Gold V] Title: 개똥벌레, Time: 316 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -0,0 +1,31 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+N, H = map(int, input().split())
+
+tops = [0] * (H+1)
+bots = [0] * (H+1)
+    
+for i in range(N):
+    num = int(input())
+    if i % 2 == 0:
+        bots[num] += 1
+    else:
+        tops[H-num+1] += 1
+            
+            
+mn = int(1e12)
+mn_num = 0
+            
+cnt = N // 2
+for h in range(1, H+1):
+    cnt -= bots[h-1]
+    cnt += tops[h]
+    
+    if mn == cnt:
+        mn_num += 1
+    if mn > cnt:
+        mn = cnt
+        mn_num = 1
+            
+print(mn, mn_num)
