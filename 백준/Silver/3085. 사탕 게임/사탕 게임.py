fa7271f596574ab04d2e0817dfc86a089ec00769
kimjeongsoo <102466913+kimjeongsoo20190147@users.noreply.github.com>
2025-08-14 11:04:42 +0900

    [Silver II] Title: 사탕 게임, Time: 2784 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,31 +1,24 @@
-N = int(input())
-matrix = [list(input()) for _ in range(N)]
-ans = 0
-
-dy = [-1,0,1,0]
-dx = [0,1,0,-1]
-
 def get_best():
     global N, matrix
     best = 0
-    
-    # rows 탐색
+    # 모든 열 탐색
     for i in range(N):
-        bef = '-'
+        bef = 'Z'
         value = 0
         for j in range(N):
-            if bef == matrix[i][j]: # one row 탐색
+            if bef == matrix[i][j]:
                 value += 1
             else:
                 value = 1
             bef = matrix[i][j]
             best = max(best, value)
-    # columns 탐색
+            
+    # 모든 행 탐색
     for j in range(N):
-        bef = '-'
+        bef = 'Z'
         value = 0
         for i in range(N):
-            if bef == matrix[i][j]:  # one column 탐색
+            if bef == matrix[i][j]:
                 value += 1
             else:
                 value = 1
@@ -34,6 +27,13 @@ def get_best():
             
     return best
 
+
+dy = [-1,0,1,0]
+dx = [0,1,0,-1]
+
+N = int(input())
+matrix = [list(input()) for _ in range(N)]
+ans = 0
 
 for y in range(N):
     for x in range(N):
@@ -46,5 +46,6 @@ for y in range(N):
                     ans = max(ans, get_best())
                     matrix[y][x], matrix[ny][nx] = matrix[ny][nx], matrix[y][x]
                     
+                    
+                    
 print(ans)
-                        
