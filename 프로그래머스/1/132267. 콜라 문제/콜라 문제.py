df31693bf62b40c3ecae9530fdbb2df0aaa13b47
kimjeongsoo <102466913+kimjeongsoo20190147@users.noreply.github.com>
2025-09-23 13:49:03 +0900

    [level 1] Title: 콜라 문제, Time: 0.34 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,9 @@
+def solution(a, b, n):
+    answer = 0
+    while n >= a:
+        new_cola = (n//a) * b
+        answer += new_cola
+        
+        n = (n % a) + new_cola
+    
+    return answer
